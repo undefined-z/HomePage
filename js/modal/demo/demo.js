dddd7bf35caf1a48402786d35e4208e10c54bee7
zhaohongyu <zhaohongyu>
2018-04-07 20:11:03 +0800

    2018-4-7提交

--- js/modal/demo/demo.js
+++ js/modal/demo/demo.js
@@ -7,7 +7,7 @@ define(['text!../js/modal/demo/demo.html'], function () {
     var html='<iframe class="" id="iframe"  frameborder="0" height="100%" width="100%"></iframe>';
     var $html=$(html);
     function init(param) {
-        $html.attr('src','../../../githubShow/project/demo/'+param.urlParam[0]+'.html');
+        $html.attr('src','../../project/demo/'+param.urlParam[0]+'.html');
     }
     return {
         init: init,
